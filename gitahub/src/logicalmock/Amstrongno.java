feba58e1721a52e516b4f98b1684650e478df38d
Dell <Dell@DESKTOP-NGDF92A>
2023-02-01 20:47:58 +0530

    github program

---
+++ gitahub/src/logicalmock/Amstrongno.java
@@ -0,0 +1,25 @@
+package logicalmock;
+
+public class Amstrongno {
+
+public static void main(String[]args) {	
+	
+for(int i=1;i<=1000;i++) {	
+	
+	int n=i;
+	int s=0;
+	int t;
+	while(n>0) {
+	t=n%10;
+	s=(t*t*t)+s;
+	n=n/10;
+}	
+	if(s==i)
+	{
+	System.out.println(s);
+	
+	}
+	
+}	
+}
+}
